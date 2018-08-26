499e04fe8ea475ecd58dc4d7713322f5d4be1c05
ccfarm <ccfarm@163.com>
2018-08-26 13:13:02 +0800

    add something

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index/index.html')
 
+@app.route('/welcome.html')
+def welcome():
+    return render_template('index/welcome.html')
+
+# train
+@app.route('/train/picture-classifier.html')
+def picture_classifier():
+    return render_template('train/picture-classifier.html')
+
+@app.route('/train/classifier.html')
+def classifier():
+    return render_template('train/classifier.html')
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ classifier.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+list = []
+list.__s
