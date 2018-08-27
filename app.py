0541f9204381a381cccf2c1ed39602978d014594
ccfarm <ccfarm@163.com>
2018-08-27 14:28:11 +0800

    add app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding = utf-8
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -12,6 +13,11 @@ import json
 from classifier import Classifier
 from classifier import load_classifier
 import csv
+import os
+import uuid
+from os import path
+
+
 
 app = Flask(__name__)
 csv_path = 'csv/wine.csv'
@@ -70,11 +76,19 @@ def classifier_train():
             elif tmp[0] == 'max_iter':
                 tmp[1] = int(tmp[1])
             param_dict[tmp[0]] = tmp[1]
-
         classifier = Classifier(csv_path, param_dict)
-        score = classifier.get_score()
-        re = {"score":score}
-        return jsonify(re)
+        return jsonify("ok")
+
+@app.route('/classfier-test', methods=['GET', 'POST'])
+def classifier_test():
+    if request.method == 'POST':
+        f = request.files['file']
+        global csv_path
+        csv_path = "csv/" + f.filename
+        f.save(csv_path)
+        global classifier
+        re = classifier.test_classifier(csv_path)
+    return jsonify(re)
 
 @app.route('/classifier-save', methods=['GET', 'POST'])
 def classifier_save():
@@ -108,10 +122,61 @@ def classifier_predict():
         y = classifier_clf.predict(X).reshape(-1,1)
         ans = np.hstack((y, X))
         ans = pd.DataFrame(ans)
-        filepath = 'static/response.csv'
-        ans.to_csv(filepath, index=False)
+        ans.to_csv('static/response.csv', index=False)
     return app.send_static_file("response.csv")
+
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def rename_filename(old_file_name):
+    basename = os.path.basename(old_file_name)
+    name, ext = os.path.splitext(basename)
+    new_name = str(uuid.uuid1()) + ext
+    return new_name
+
+@app.route('/picture-classifier-upload', methods=['GET', 'POST'])
+def upload_pic_file():
+    if request.method == 'POST':
+        print('test hello')
+        file = request.files['file']
+        # old_file_name = file.filename
+        # if file and allowed_files(old_file_name):
+        # filename = rename_filename(old_file_name)
+        # file_path = os.path.join(UPLOAD_FOLDER, filename)
+        # file.save(file_path)
+        print(file.filename)
+        base_path = path.abspath(path.dirname(__file__))
+        upload_path = path.join(base_path, 'uploads/')
+        file_name = upload_path + file.filename
+        file.save(file_name)
+        print(file_name)
+    return redirect(url_for('picture_classifier'))
+
+
+@app.route('/picture-classifier-params', methods=['GET', 'POST'])
+def get_pic_train_params():
+    if request.method == 'POST':
+        params = {'learnRate': request.form.get('learnRateVal'), 'batchSzie': request.form.get('batchSzie'),
+                  'checkPointPath': request.form.get('checkPointPath'), 'trainDir': request.form.get('trainDir'),
+                  'excludeScopes': request.form.get('excludeScopes'), 'trainScopes': request.form.get('trainScopes'),
+                  'optimizer': request.form.get('optimizer'), 'datasetName': request.form.get('datasetName'),
+                  'modelName': request.form.get('modelName')}
+        print(params)
+    # base_path = path.abspath(path.dirname(__file__))
+    with open('./uploads/file.txt', 'rb') as f:
+        data_path = f.read().strip()
+    cmd = """python train_image_classifier.py --dataset_name=cifar10 --dataset_dir=./slim/tmp/cifar10 
+    --checkpoint_path=./slim/tmp/pnasnet-5_large_2017_12_13/model.ckpt 
+    --checkpoint_exclude_scopes=final_layer,aux_7 --trainable_scopes=final_layer,aux_7,cell10,cell11 
+    --model_name=pnasnet_large --train_dir=./slim/tmp/pnasnet-model --learning_rate=0.001 
+    --optimizer=rmsprop --batch_size=8 --clone_on_cpu=True"""
+    os.system(cmd)
+    return redirect(url_for('picture_classifier'))
 
 if __name__ == '__main__':
     app.debug=True
     app.run()
+
+
+
--- classifier.py
+++ classifier.py
@@ -12,13 +12,18 @@ class Classifier:
         data = pd.read_csv(csv_path)
         X = data.iloc[:, 1:]
         y = data.iloc[:, 0]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
         self.clf = LogisticRegression(**param_dict)
-        self.clf.fit(X_train, y_train)
-        self.score = self.clf.score(X_test, y_test)
+        self.clf.fit(X, y)
 
-    def get_score(self):
-        return self.score
+    def test_classifier(self, csv_path):
+        data = pd.read_csv(csv_path)
+        X = data.iloc[:, 1:]
+        y = data.iloc[:, 0]
+        yy = self.clf.predict(X)
+        y = list(y)
+        yy = list(yy)
+        print y
+        print yy
 
     def save_clf(self, name):
         model_path = "model/" + name + ".m"
@@ -33,13 +38,6 @@ class Classifier:
         conn.close()
         return None
 
-
-    # def predict(self, csv):
-    #     X = pd.read_csv()
-    #     y = self.clf.predict(X)
-    #     data = pd.concat(y, X, axis=1)
-    #     data.to_csv()
-
 def load_classifier(id):
     conn = mysql.connector.connect(user=const.db_user_name, password=const.db_password, database=const.db_database
                                    , auth_plugin='mysql_native_password')
@@ -50,5 +48,3 @@ def load_classifier(id):
     conn.close()
     path = model[4]
     return joblib.load(path)
-
-
